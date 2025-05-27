54042fa3809af2faf34066a64b88c107edd0aaa3
44mira <legolaslada@gmail.com>
2025-05-28 02:30:16 +0800

    chore: init

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use raylib::prelude::*;
+
+fn main() {
+  let (mut rl, thread) = raylib::init().title("Turing Cogs").vsync().build();
+
+  while !rl.window_should_close() {
+    let mut d = rl.begin_drawing(&thread);
+
+    d.clear_background(Color::WHITE);
+    d.draw_text(
+      "Hello world",
+      (d.get_render_width() - d.measure_text("Hello world", 24)) / 2,
+      d.get_render_height() / 2,
+      24,
+      Color::BLACK,
+    );
+  }
+}
