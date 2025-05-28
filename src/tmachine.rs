640b472bd88a9fccf72bf2808a873fad39254289
44mira <legolaslada@gmail.com>
2025-05-28 16:35:41 +0800

    feat: memory tape stepping

--- src/tmachine.rs
+++ src/tmachine.rs
@@ -2,6 +2,11 @@
 
 use std::collections::HashMap;
 
+enum Direction {
+  Left,
+  Right,
+}
+
 struct TuringMachine {
   memory_tape: Vec<char>,
 
@@ -10,6 +15,8 @@ struct TuringMachine {
 
   // lookups are assigned by name and input, to reduce hashmap depth
   lookup: HashMap<(String, char), String>,
+
+  tape_pointer: usize,
 }
 
 impl TuringMachine {
@@ -22,8 +29,35 @@ impl TuringMachine {
 
     TuringMachine {
       current_state: "START".to_owned(),
+      tape_pointer: 0,
       memory_tape,
       lookup,
+    }
+  }
+
+  /// Reads the memory tape at the tape pointer, returning the found character.
+  fn read(&self) -> char {
+    self.memory_tape[self.tape_pointer]
+  }
+
+  /// Writes the character `e` in the memory tape at the tape pointer.
+  fn write(&mut self, e: char) {
+    self.memory_tape[self.tape_pointer] = e;
+  }
+
+  /// Updates the character found at the tape pointer using `updater`, and then
+  /// moves the tape pointer one step to the specified Direction in `dir`.
+  fn step<F>(&mut self, updater: F, dir: Direction)
+  where
+    F: FnOnce(char) -> char,
+  {
+    // update the tape
+    self.write(updater(self.read()));
+
+    // move pointer
+    match dir {
+      Direction::Right => self.tape_pointer += 1,
+      Direction::Left => self.tape_pointer -= 1,
     }
   }
 }
@@ -34,6 +68,22 @@ mod tests {
 
   #[test]
   fn tm_init() {
-    let _tm = TuringMachine::new();
+    let tm = TuringMachine::new();
+
+    assert_eq!("START", tm.current_state);
+    assert_eq!(0, tm.tape_pointer);
+    assert_eq!(2048, tm.memory_tape.capacity());
+  }
+
+  #[test]
+  fn tm_step() {
+    let mut tm = TuringMachine::new();
+    tm.memory_tape = "abba".chars().collect();
+
+    assert_eq!('a', tm.read());
+    tm.step(|_| '#', Direction::Right);
+    assert_eq!('b', tm.read());
+    tm.step(|x| x, Direction::Left);
+    assert_eq!('#', tm.read());
   }
 }
